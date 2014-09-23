29dc01c1d73bcf2492b8b1979d96f94702ae9243
d-smith <doug.smith.mail@gmail.com>
2014-09-23 14:39:58 -0600

    Added some unit tests for partial fn, currying, composition

--- src/main/scala/gettingstarted/Currying.scala
+++ src/main/scala/gettingstarted/Currying.scala
@@ -1,6 +1,6 @@
 package gettingstarted
 
-class Currying {
+object Currying {
   def partial1[A,B,C](a:A, f:(A,B)=>C):B => C = (b:B) => f(a,b)
 
   def curry[A,B,C](f:(A,B) =>C):A => (B => C) = (a: A) => partial1(a,f)
---
+++ src/test/scala/gettingstarted/PartialAndCurryingTest.scala
@@ -0,0 +1,29 @@
+package gettingstarted
+
+import org.scalatest.FlatSpec
+
+
+class PartialAndCurryingTest extends FlatSpec {
+  import gettingstarted.Currying.partial1
+  import gettingstarted.Currying.curry
+  import gettingstarted.Currying.compose
+
+  def add(a:Int, b:Int) : Int = a + b
+  def printSum(sum: Int) : String = s"sum is $sum"
+
+  "Partially applied functions" can "be completed" in {
+    val inc = partial1(1, add)
+    assert(inc(2) === 3)
+  }
+
+  "Curried functions" can "be applied" in {
+    val curried = curry(add)
+    assert(curried(1)(2) === 3)
+  }
+
+  "Functions" can "be composed using compose" in {
+    val inc = partial1(1, add)
+    val composed = compose(printSum, inc)
+    assert(composed(3) === "sum is 4")
+  }
+}
