d402845775333c2219febd626c7dd5ab16f2d81c
d-smith <doug.smith.mail@gmail.com>
2014-09-25 06:56:37 -0600

    Added fds package and List

---
+++ src/main/scala/fds/List.scala
@@ -0,0 +1,17 @@
+package fds
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def apply[A](as: A*) : List[A] = if(as.isEmpty) Nil else Cons(as.head, apply(as.tail : _*))
+
+  def tail[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(_,Nil) => Nil
+      case Cons(h, t) => t
+    }
+  }
+}
