b33b2a4bb6bbd2023856ef4ea578ab85e8c40eed
d-smith <doug.smith.mail@gmail.com>
2014-10-07 07:26:20 -0600

    Added maximum function to Tree

--- src/test/scala/fds/TreeTest.scala
+++ src/test/scala/fds/TreeTest.scala
@@ -10,5 +10,10 @@ class TreeTest extends WordSpec with MustMatchers {
       val t:Tree[Int] = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
       assert(Tree.size(t) === 5)
     }
+
+    "return its max" in {
+      val t:Tree[Int] = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
+      assert(Tree.maximum(t) === 3)
+    }
   }
 }
