cdc16cafd6c116a5a4295992b07baef21db968c1
d-smith <doug.smith.mail@gmail.com>
2014-10-15 07:08:20 -0600

    Added variance exercise

---
+++ src/main/scala/errhandling/Variance.scala
@@ -0,0 +1,13 @@
+package errhandling
+
+
+object Variance {
+
+  def mean(xs: Seq[Double]) : Option[Double] = {
+    if(xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
+  def variance(xs: Seq[Double]) : Option[Double] = {
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
+}
