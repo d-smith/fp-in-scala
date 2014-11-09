3807a37735829047638aa55fd3c366387b596c7f
d-smith <doug.smith.mail@gmail.com>
2014-11-09 06:37:17 -0700

    Adde RNG

---
+++ src/main/scala/state/RNG.scala
@@ -0,0 +1,19 @@
+package state
+
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+  case class Simple(seed: Long) extends RNG {
+    //nextInt algorithm - http://en.wikipedia.org/wiki/Linear_congruential_generator
+    //implementation from Functional Programming in Scala
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+}
