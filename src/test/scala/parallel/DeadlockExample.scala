b40659e38a056107b3ab975d4b532475668d8940
d-smith <doug.smith.mail@gmail.com>
2014-11-28 06:52:22 -0700

    added a comment to the deadlock example

--- src/test/scala/parallel/DeadlockExample.scala
+++ src/test/scala/parallel/DeadlockExample.scala
@@ -2,7 +2,8 @@ package parallel
 
 import java.util.concurrent.Executors
 
-
+//This deadlocks because there's only a single thread in the pool, and we
+//are submitting a callable within a callable, e.g. fork(a)
 object DeadlockExample extends App {
   import Par._
   val a = lazyUnit(42 + 1)
