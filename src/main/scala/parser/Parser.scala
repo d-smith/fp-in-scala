b7c07ff879c32b0e5497d4e275e021759b9d2b86
d-smith <doug.smith.mail@gmail.com>
2014-12-16 07:22:40 -0700

    Added asStringParser, and listOfN prototype

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -5,8 +5,13 @@ package parser
 trait Parsers[ParseError, Parser[+_]] { self =>
   def char(c: Char) : Parser[Char]
   implicit def string(s: String): Parser[String]
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]) : ParserOps[String] = ParserOps(f(a))
+
   def or[A](s1: Parser[A], s2: Parser[A]) : Parser[A]
   def run[A](p: Parser[A])(input:String) : Either[ParseError, A]
+
+  def listOfN[A](n: Int, p : Parser[A]) : Parser[List[A]] = ???
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]) : Parser[B] = self.or(p,p2)
