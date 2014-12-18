574ee1f84ea44470758eaaab641a47c4a1750a86
d-smith <doug.smith.mail@gmail.com>
2014-12-18 07:01:19 -0700

    Added map, many, succeed, laws

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -1,9 +1,14 @@
 package parser
 
+import proptesting._
+import proptesting.Prop._
 
 
 trait Parsers[ParseError, Parser[+_]] { self =>
-  def char(c: Char) : Parser[Char]
+  def char(c: Char) : Parser[Char] = string(c.toString).map(_.charAt(0))
+
+  def succeed[A](a:A) : Parser[A] = string("").map(_ => a)
+
   implicit def string(s: String): Parser[String]
 
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]) : ParserOps[String] = ParserOps(f(a))
@@ -13,12 +18,24 @@ trait Parsers[ParseError, Parser[+_]] { self =>
 
   def listOfN[A](n: Int, p : Parser[A]) : Parser[List[A]] = ???
 
+  def map[A,B](p: Parser[A])(f: A => B) : Parser[B] = ???
+
+  def many[A](p: Parser[A]) : Parser[List[A]] = ???
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]) : Parser[B] = self.or(p,p2)
     def or[B>:A](p2: => Parser[B]) : Parser[B] = self.or(p,p2)
 
-    def map[A,B](p: Parser[A])(f: A => Parser[B]) : Parser[B] = ???
+    def map[B](f: A => B) : Parser[B] = self.map(p)(f)
 
-    def many[A](p: Parser[A]) : Parser[List[A]] = ???
+    def many = self.many(p)
+  }
+
+  object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in:Gen[String]) : Prop =
+      Gen.forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]) : Prop =
+      equal(p, map(p)(a => a))(in)
   }
 }
