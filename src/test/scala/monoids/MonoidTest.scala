0bf63b62cbab3584fa3479c937215f8915713126
d-smith <doug.smith.mail@gmail.com>
2014-12-21 07:02:28 -0700

    Added rest of monoid sample tests

--- src/test/scala/monoids/MonoidTest.scala
+++ src/test/scala/monoids/MonoidTest.scala
@@ -4,9 +4,10 @@ import org.scalatest.WordSpec
 import org.scalatest.matchers.MustMatchers
 
 class MonoidTest extends WordSpec with MustMatchers {
-  import Monoid.stringMonoid.op
-  import Monoid.stringMonoid.zero
+
   "the string monoid obeys the monoid laws" in {
+    import Monoid.stringMonoid.op
+    import Monoid.stringMonoid.zero
     assert(op(op("x","y"), "z") === op("x", op("y","z")))
     assert(op("a", zero) === "a")
   }
@@ -20,6 +21,40 @@ class MonoidTest extends WordSpec with MustMatchers {
 
     assert(listMonoid[Int].op(listMonoid[Int].op(x,y),z) === listMonoid[Int].op(x,listMonoid[Int].op(y,z)))
     assert(listMonoid[Int].op(x, listMonoid[Int].zero) === x)
+  }
+
+  "the int addition monoid obeys the monadic laws" in {
+    import Monoid.intAddition.op
+    import Monoid.intAddition.zero
+
+    assert(op(op(1,2), 3) === op(1, op(2,3)))
+    assert(op(10, zero) === 10)
+  }
+
+  "the int multiplication monoid obeys the monadic laws" in {
+    import Monoid.intMultiplication.op
+    import Monoid.intMultiplication.zero
+
+    assert(op(op(1,2), 3) === op(1, op(2,3)))
+    assert(op(10, zero) === 10)
+  }
+
+  "the boolean and monoid obeys the Monadic laws" in {
+    import Monoid.booleanAnd.op
+    import Monoid.booleanAnd.zero
+
+    assert(op(op(true,true), true) === op(true, op(true,true)))
+    assert(op(false, zero) === false)
+    assert(op(true, zero) === true)
+  }
+
+  "the boolean or monoid obeys the Monadic laws" in {
+    import Monoid.booleanOr.op
+    import Monoid.booleanOr.zero
+
+    assert(op(op(true,false), true) === op(true, op(false,true)))
+    assert(op(false, zero) === false)
+    assert(op(true, zero) === true)
   }
 
 
