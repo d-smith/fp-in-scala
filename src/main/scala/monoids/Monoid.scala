029b64cd59c23ab8389febdb94d328aa684e38c1
d-smith <doug.smith.mail@gmail.com>
2014-12-22 07:10:23 -0700

    Added foldMap to Monoid

--- src/main/scala/monoids/Monoid.scala
+++ src/main/scala/monoids/Monoid.scala
@@ -46,4 +46,7 @@ object Monoid {
     override def zero: (A) => A = (a:A) => a
     override def op(a1: (A) => A, a2: (A) => A): (A) => A = a1.compose(a2)
   }
+
+  def foldMap[A,B](as: List[A], m: Monoid[B])(f: A => B) : B =
+    as.foldLeft(m.zero)((b,a) => m.op(b,f(a)))
 }
