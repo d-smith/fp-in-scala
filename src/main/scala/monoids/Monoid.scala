fdec6974674d806bd1fe5e4f9e3272ca4930a97a
d-smith <doug.smith.mail@gmail.com>
2014-12-28 10:35:04 -0700

    Added OptionFoldable and TreeFoldable

--- src/main/scala/monoids/Monoid.scala
+++ src/main/scala/monoids/Monoid.scala
@@ -116,3 +116,47 @@ object StreamFoldable extends Foldable[Stream] {
   override def foldMap[A, B](as: Stream[A])(f: (A) => B)(mb: Monoid[B]): B =
     foldLeft(as)(mb.zero)((b,a) => mb.op(b,f(a)))
 }
+
+object OptionFoldable extends Foldable[Option] {
+  override def foldRight[A, B](as: Option[A])(z: B)(f: (A, B) => B): B =
+    as match {
+      case None => z
+      case Some(a) => f(a,z)
+    }
+
+  override def foldLeft[A, B](as: Option[A])(z: B)(f: (B, A) => B): B =
+    as match {
+      case None => z
+      case Some(a) => f(z, a)
+    }
+
+  override def foldMap[A, B](as: Option[A])(f: (A) => B)(mb: Monoid[B]): B =
+    as match {
+      case None => mb.zero
+      case Some(a) => f(a)
+    }
+}
+
+sealed trait Tree[+A]
+case class Leaf[A](value:A) extends Tree[A]
+case class Branch[A](left:Tree[A],right:Tree[A]) extends Tree[A]
+
+object TreeFoldable extends Foldable[Tree] {
+  override def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B): B =
+    as match {
+      case Leaf(a) => f(a,z)
+      case Branch(l,r) => foldRight(l)(foldRight(r)(z)(f))(f)
+    }
+
+  override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B =
+    as match {
+      case Leaf(a) => f(z,a)
+      case Branch(l,r) => foldLeft(r)(foldLeft(r)(z)(f))(f)
+    }
+
+  override def foldMap[A, B](as: Tree[A])(f: (A) => B)(mb: Monoid[B]): B =
+    as match {
+      case Leaf(a) => f(a)
+      case Branch(l,r) => mb.op(foldMap(l)(f)(mb), foldMap(r)(f)(mb))
+    }
+}
