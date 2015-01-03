23c07219659b314d3a7b7646c358ecc2dcf70cbc
d-smith <doug.smith.mail@gmail.com>
2015-01-03 10:03:56 -0700

    Defined Monod trait

---
+++ src/main/scala/monads/Monad.scala
@@ -0,0 +1,13 @@
+package monads
+
+trait Monad[F[_]] extends Functor[F] {
+  def unit[A](a: => A) : F[A]
+
+  def flatMap[A,B](ma: F[A])(f: A => F[B]) : F[B]
+
+  def map[A,B](ma: F[A])(f: A => B) =
+    flatMap(ma)(a => unit(f(a)))
+
+  def map2[A,B,C](ma: F[A], mb : F[B])(f: (A,B) => C) : F[C] =
+    flatMap(ma)(a => map(mb)(b => f(a,b)))
+}
