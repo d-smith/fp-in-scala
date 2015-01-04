9a71fbe169265b6720769d8f8527fe2800262699
d-smith <doug.smith.mail@gmail.com>
2015-01-04 06:39:31 -0700

    Added option, list, and stream monads

--- src/main/scala/monads/Monad.scala
+++ src/main/scala/monads/Monad.scala
@@ -11,3 +11,20 @@ trait Monad[F[_]] extends Functor[F] {
   def map2[A,B,C](ma: F[A], mb : F[B])(f: (A,B) => C) : F[C] =
     flatMap(ma)(a => map(mb)(b => f(a,b)))
 }
+
+object Monad {
+  val optionMonad = new Monad[Option] {
+    override def unit[A](a: => A): Option[A] = Some(a)
+    override def flatMap[A, B](ma: Option[A])(f: (A) => Option[B]): Option[B] = ma flatMap f
+  }
+
+  val listMonad = new Monad[List] {
+    override def unit[A](a: => A): List[A] = List(a)
+    override def flatMap[A, B](ma: List[A])(f: (A) => List[B]): List[B] = ma flatMap f
+  }
+
+  val streamMonad = new Monad[Stream] {
+    override def unit[A](a: => A): Stream[A] = Stream(a)
+    override def flatMap[A, B](ma: Stream[A])(f: (A) => Stream[B]): Stream[B] = ma flatMap f
+  }
+}
