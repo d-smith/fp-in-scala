b425c01063d69ec1e39f7b2f7c216e29a2e80c70
d-smith <doug.smith.mail@gmail.com>
2015-01-07 07:30:45 -0700

    Added flatMap via compose, Id, and Id Monad

--- src/main/scala/monads/Monad.scala
+++ src/main/scala/monads/Monad.scala
@@ -25,6 +25,9 @@ trait Monad[F[_]] extends Functor[F] {
   def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] =
     a => flatMap(f(a))(g)
 
+  def flatMapViaCompose[A,B](ma: F[A])(f: A => F[B]) : F[B] =
+    compose((_:Unit) => ma,f)(())
+
   def filterM[A](ms: List[A])(f: A => F[Boolean]) : F[List[A]] =
     ms.foldRight(unit(List[A]()))((x,y) =>
       compose(f, (b: Boolean) => if(b) map2(unit(x),y)(_::_) else y)(x))
@@ -45,4 +48,16 @@ object Monad {
     override def unit[A](a: => A): Stream[A] = Stream(a)
     override def flatMap[A, B](ma: Stream[A])(f: (A) => Stream[B]): Stream[B] = ma flatMap f
   }
+
+  val idMonad = new Monad[Id] {
+    override def unit[A](ida: => A): Id[A] = Id(ida)
+    override def flatMap[A,B](ida: Id[A])(f: A => Id[B]) : Id[B] = ida flatMap f
+  }
 }
+
+case class Id[A](value: A) {
+  def map[B](ida: Id[A])(f: A => B) : Id[B] = Id(f(ida.value))
+  def flatMap[B](f: A => Id[B]) : Id[B] = f(value)
+}
+
+
